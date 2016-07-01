5880bc7fe7eb274c1b0ca7cab86b54c0e6025934
Liam Nichols <liam.nichols@rockpooldigital.com>
2016-07-01 22:24:55 +0100

    added the ability to read the project data. Moved out of a directory based module into just a single file

---
+++ build/project-data.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+/// Reads a project.json file from within the specified directory and returns it
+exports.read = function(workspace) {
+    
+    // get the absolute path to the json file
+    var filepath = path.join(workspace, 'project.json')
+    
+    // read the file
+    var fs = require('fs')
+    var file = fs.readFileSync(filepath, 'utf8')
+    
+    // parse the json
+    var json = JSON.parse(file)
+    
+    // return the json
+    return json
+}
