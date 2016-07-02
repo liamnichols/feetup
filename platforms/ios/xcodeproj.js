a8d3dced4ad76240dd43ce9076c9b572fdd5944d
Liam Nichols <liam.nichols@rockpooldigital.com>
2016-07-02 22:38:58 +0100

    xcodeproj.js parsing of schemes now exposes the 'entries' (targets) built within a scheme

--- platforms/ios/xcodeproj.js
+++ platforms/ios/xcodeproj.js
@@ -39,6 +39,7 @@ function resolveSchemesInDirectory(dir) {
     return schemes
 } 
 
+
 function parseScheme(schemePath) {
     
     // read the scheme data into a string
@@ -67,12 +68,80 @@ function parseScheme(schemePath) {
         path: schemePath,
         name: path.basename(schemePath, ".xcscheme"),
         actions: {
-            test: {
-                configuration: xml.Scheme.TestAction[0].$.buildConfiguration
-            }, 
-            archive: {
-                configuration: xml.Scheme.ArchiveAction[0].$.buildConfiguration
-            }
+            build: parseBuildActionFromXML(xml.Scheme.BuildAction[0], true),
+            run: parseBuildActionFromXML(xml.Scheme.LaunchAction[0], false),
+            test: parseBuildActionFromXML(xml.Scheme.TestAction[0], false),
+            profile: parseBuildActionFromXML(xml.Scheme.ProfileAction[0], false),
+            analyze: parseBuildActionFromXML(xml.Scheme.AnalyzeAction[0], false),
+            archive: parseBuildActionFromXML(xml.Scheme.ArchiveAction[0], false)
         }
+    }
+}
+
+function parseBuildActionFromXML(actionXML, isBuild) {
+    
+    // the action object
+    var action = { }
+    
+    // check if this is the build action type
+    if (isBuild == true) {
+        
+        // build specific bits
+        action.parallelizeBuildables = boolFromXML(actionXML.$.parallelizeBuildables)
+        action.buildImplicitDependencies = boolFromXML(actionXML.$.buildImplicitDependencies)
+        action.entries = parseBuildActionEntriesFromXML(actionXML.BuildActionEntries)
+        
+    } else {
+        
+        // set the configuration, this is consistent across every action apart from build
+        action.configuration = actionXML.$.buildConfiguration
+    }
+    
+    // return the action
+    return action
+}
+
+function parseBuildActionEntriesFromXML(actionEntriesXML) {
+    
+    // somewhere to store the parsed entries
+    var entries = Array()
+    
+    // enumerate each entry
+    for (entryXML of actionEntriesXML[0].BuildActionEntry) {
+        
+        // get the elements
+        var referenceXML = entryXML.BuildableReference[0].$
+        var entry = { }
+        
+        // read the build for flags
+        entry.buildsFor = {
+            testing: boolFromXML(entryXML.$.buildForTesting),
+            running: boolFromXML(entryXML.$.buildForRunning),
+            profiling: boolFromXML(entryXML.$.buildForProfiling),
+            archiving: boolFromXML(entryXML.$.buildForArchiving),
+            analyzing: boolFromXML(entryXML.$.buildForAnalyzing)
+        }
+        
+        // read the target settings
+        entry.target = {
+            name: referenceXML.BlueprintName,
+            identifier: referenceXML.BlueprintIdentifier
+        }
+        
+        // add the entry to the Array
+        entries.push(entry)
+    }
+    
+    // return the parsed entries
+    return entries
+}
+
+function boolFromXML(xmlString) {
+    if (xmlString == "YES") {
+        return true
+    } else if (xmlString == "NO") {
+        return false
+    } else {
+        return null
     }
 }
