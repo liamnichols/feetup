5efa0311d06dd3205ec4777e88c636cbca51849c
Liam Nichols <liam.nichols@rockpooldigital.com>
2016-07-02 15:35:29 +0100

    added the other xcodebuild commands

--- platforms/ios/xcodebuild.js
+++ platforms/ios/xcodebuild.js
@@ -22,19 +22,68 @@ exports.build = function(workspacePath, schemeName, configuration, derivedDataPa
 
 exports.test = function(workspacePath, schemeName, configuration, derivedDataPath, destinations, shouldClean) {
     
-    console.log("xcodebuild test")
+    // build the arguments
+    var args = [
+        "test",
+        "-workspace", workspacePath,
+        "-scheme", schemeName,
+        "-configuration", configuration,
+        "-derivedDataPath", derivedDataPath
+    ]
+    
+    // parse the destination strings
+    for (destination of destinations) {
+        
+        // add each destination
+        args.push("-destination")
+        args.push(destinationArgFromObject(destination))
+    }
+    
+    // add the optional clear arg
+    if (shouldClean) {
+        args.splice(0, 0, "clean") // insert it at the font
+    }
+    
+    // execute the command
+    xcodebuildWithArgs(args)
 }
 
 exports.archive = function(workspacePath, schemeName, configuration, derivedDataPath, archivePath, shouldClean) {
     
-    console.log("xcodebuild archive")
-}
-
-exports.exportArchive = function(archivePath, exportPath, exportOptions) {
+    // build the arguments
+    var args = [
+        "archive",
+        "-workspace", workspacePath,
+        "-scheme", schemeName,
+        "-configuration", configuration,
+        "-derivedDataPath", derivedDataPath,
+        "-archivePath", archivePath
+    ]
     
-    console.log("xcodebuild -exportArchive")
+    // add the optional clear arg
+    if (shouldClean) {
+        args.splice(0, 0, "clean") // insert it at the font
+    }
+    
+    // execute the command
+    xcodebuildWithArgs(args)
 }
 
+exports.exportArchive = function(archivePath, exportPath, exportOptionsPlist) {
+    
+    // build the arguments
+    var args = [
+        "-exportArchive",
+        "-archivePath", archivePath,
+        "-exportPath", exportPath,
+        "-exportOptionsPlist", exportOptionsPlist,
+    ]
+    
+    // execute the command
+    xcodebuildWithArgs(args)
+}
+
+/// Exectues the xcodebuild command with the specified arguments
 function xcodebuildWithArgs(args) {
     
     // log it
@@ -45,4 +94,11 @@ function xcodebuildWithArgs(args) {
     child_process.spawnSync("xcodebuild", args, {
         stdio: [ 0, 1, 2 ]
     })
+}
+
+/// Helper for test destinations
+function destinationArgFromObject(obj) {
+    
+    // build a platform string to be passed into the cli
+    return "platform=" + obj.platform + ",name=" + obj.name + ",OS=" + obj.version
 }
